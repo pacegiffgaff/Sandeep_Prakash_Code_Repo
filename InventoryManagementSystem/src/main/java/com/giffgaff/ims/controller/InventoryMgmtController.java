e3eed564978a00927476db07bf7b82e664695180
sandeep <sandeepamilineni@gmail.com>
2020-03-16 01:17:15 +0530

    Inventory Tracking - InventoryMgmtController.java

--- InventoryManagementSystem/src/main/java/com/giffgaff/ims/controller/InventoryMgmtController.java
+++ InventoryManagementSystem/src/main/java/com/giffgaff/ims/controller/InventoryMgmtController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class RawMaterialsController {
+public class InventoryMgmtController {
     @Autowired
     InventoryService inventoryService;
 
