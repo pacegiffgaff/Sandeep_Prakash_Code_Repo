90f8370aea81e01660e0252c7795a8f75ec10a06
Prakash Badisa <Prakash.badisa@nttdata.com>
2020-03-19 12:32:16 +0530

    updated testcases

--- InventoryManagementSystem/src/test/java/com/giffgaff/ims/service/InventoryMgmtServiceImplTest.java
+++ InventoryManagementSystem/src/test/java/com/giffgaff/ims/service/InventoryMgmtServiceImplTest.java
@@ -1,27 +1,54 @@
 package com.giffgaff.ims.service;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.giffgaff.ims.dao.ProductDAO;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.giffgaff.ims.model.Product;
+import com.giffgaff.ims.service.impl.InventoryMgmtServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
 public class InventoryMgmtServiceImplTest {
 
-	public void testAddProduct() {
 
-		fail("Service failed");
-	}
+	@Mock
+	ProductDAO productDAO;
 
-	public void testGetProductById() {
+	@InjectMocks
+	InventoryMgmtServiceImpl inventoryMgmtServiceImpl;
 
-		fail("Service failed");
-	}
-
+	
+	@Test
 	public void testGetListOfProdcts() {
-
-		fail("Service failed");
+		
+		List<Product> productList = new ArrayList<Product>();
+		Product product = new Product();		
+		product.setProductName("AppleJuice");
+		product.setProductId(1L);
+		product.setProductType("Juices");
+		
+		Product productobj = new Product();
+		productobj.setProductName("AppleJuice");
+		productobj.setProductId(1L);
+		productobj.setProductType("Juices");
+		
+		productList.add(product);
+		productList.add(productobj);
+		when(productDAO.findAll()).thenReturn(productList);
+		
+		
+		assertEquals(2, productList.size());
 	}
-
-	public void testUpdateProductById() {
-
-		fail("Service failed");
-	}
-
+	
 }
